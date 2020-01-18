ffca63c477081bf9aaf825a3f9271a9a7c87c373
Anton Dyshkant <vyshkant@gmail.com>
2020-01-18 17:12:34 +0300

    Bibliographic record repository

---
+++ src/Persistence/Repository/BibliographicRecordRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of VyfonyBibliographyBundle project.
+ *
+ * (c) Anton Dyshkant <vyshkant@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Vyfony\Bundle\BibliographyBundle\Persistence\Repository;
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Vyfony\Bundle\BibliographyBundle\Persistence\Entity\BibliographicRecord;
+
+/**
+ * @author Anton Dyshkant <vyshkant@gmail.com>
+ */
+class BibliographicRecordRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, BibliographicRecord::class);
+    }
+}
